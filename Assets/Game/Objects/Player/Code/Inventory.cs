a1fcf43bc3f5161aebc975ae389e9281a4f8a76c
fritzmann2 <gagabais994@gmail.com>
2025-12-15 16:49:24 +0100

    feat: Add OpenInventory and SummonWeapon actions to GameControls and implement functionality in Inventory

--- Assets/Game/Objects/Player/Code/Inventory.cs
+++ Assets/Game/Objects/Player/Code/Inventory.cs
@@ -2,15 +2,53 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private GameControls controls;
+    public GameObject[] inventory;
+    private GameObject currentWeapon;
+    public GameObject Handpoint;
+
+    void OnEnable()
     {
-        
+        controls.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnDisable()
     {
-        
+        controls.Disable();
     }
+
+    void Awake()
+    {
+        controls = new GameControls();
+    }
+
+    void FixedUpdate()
+    {
+        if (controls.Gameplay.OpenInventory.triggered)
+        {
+            ToggleInventory();
+        }
+        if (controls.Gameplay.SummonWeapon.triggered)
+        {
+            SummonWeapon();
+        }
+    }
+
+    private void ToggleInventory()
+    {
+        Debug.Log("Toggling Inventory");
+    }
+    private void SummonWeapon()
+    {
+        if (currentWeapon == null && inventory.Length > 0)
+        {
+            currentWeapon = Instantiate(inventory[0], Handpoint.transform.position, Quaternion.identity, transform);
+        }
+        else if (currentWeapon != null)
+        {
+            Destroy(currentWeapon);
+            currentWeapon = null;
+        }
+    }
+
 }
